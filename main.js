d39fa8cbb5d654a4a3045e2bb6b573574d817d4d
Johno Caldwell <jjcldwll@gmail.com>
2022-04-04 18:58:51 -0400

    Create math functions

---
+++ main.js
@@ -0,0 +1,11 @@
+// Addition function
+const addition = (a, b) => a + b;
+
+// Subtraction function
+const subtraction = (a, b) => a - b;
+
+// Multiplication function
+const multiplication = (a, b) => a * b;
+
+// Division function
+const division = (a, b) => a / b;
